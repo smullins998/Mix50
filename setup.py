efa726616b74fec31944ddc8b722d0e5ace5586c
Sean Mullins <133051216+smullins998@users.noreply.github.com>
2024-08-30 15:39:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# with open('README.md', encoding='utf8') as f:
-#     long_description = f.read()
+with open('README.md', encoding='utf8') as f:
+    long_description = f.read()
 
 setup(
     name='Mix50',
     version='0.4',
     description='Fast and simple DJ and audio effects in Python with Librosa',
-    # long_description=long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 4 - Beta',  # Updated classifier
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.11',
         'Topic :: Multimedia :: Sound/Audio :: Analysis'
@@ -19,7 +19,7 @@ setup(
     author='Sean Mullins',
     author_email='smullins998@gmail.com',
     license='MIT',
-    packages=['Mix50'],
+    packages=find_packages(),
     install_requires=[
         'librosa>=0.10.2.post1',
         'numpy<2',
@@ -34,6 +34,10 @@ setup(
         'matplotlib',
         'sounddevice',
     ],
+    extras_require={
+        'ffmpeg': ['ffmpeg-python'],
+        'portaudio': ['pyaudio'],
+    },
     keywords=['python', 'audio', 'DSP', 'DJ', 'audio analysis'],
     include_package_data=True,
     zip_safe=False
